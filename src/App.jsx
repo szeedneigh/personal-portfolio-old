aa29ebc088bb47fa28c0277396b8d6dd2bb16804
szeedneigh <sidneyjohn1704@gmail.com>
2024-10-17 23:03:55 +0800

    Added React components and modified the navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,13 @@
 
+import Navbar from './components/Navbar.jsx'
+
+
 function App() {
   return (
     <>
 
-{/* <div id="preloader">
-        <div id="status">
-          <div className="spinner">
-            <div className="cube1" />
-            <div className="cube2" />
-          </div>
-        </div>
-      </div> */}
+      <Navbar />
 
-      {/* START NAVBAR */}
-      <nav
-        id="navbar"
-        className="navbar navbar-expand-lg nav-light fixed-top sticky"
-      >
-        <div className="container">
-          <a className="navbar-brand" href="#">
-            <span className="logo-light-mode">
-              <img src="images/logo-dark.png" className="l-dark" alt="" />
-              <img src="images/logo-light.png" className="l-light" alt="" />
-            </span>
-            <img
-              src="images/logo-light.png"
-              className="logo-dark-mode"
-              alt=""
-            />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <i data-feather="menu" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0" id="navbar-navlist">
-              <li className="nav-item">
-                <a className="nav-link" href="#home">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#about">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#resume">
-                  Resume
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#portfolio">
-                  Portfolio
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#services">
-                  Services
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#blog">
-                  Blog
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-          {/*end collapse*/}
-        </div>
-        {/*end container*/}
-      </nav>
-      {/* END NAVBAR */}
       {/* start hero */}
       <section
         className="bg-half-135 bg-dark pb-0 align-items-center hero-wrapper overflow-hidden position-relative"
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+// components/Navbar.js
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav
+    id="navbar"
+    className="navbar navbar-expand-lg nav-light fixed-top sticky"
+    >
+    <div className="container">
+      <a className="navbar-brand" href="#">
+        <span className="logo-light-mode">
+          <img src="images/logo-dark.png" className="l-dark" alt="" />
+          <img src="images/logo-light.png" className="l-light" alt="" />
+        </span>
+        <img
+          src="images/logo-light.png"
+          className="logo-dark-mode"
+          alt=""
+        />
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i data-feather="menu" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0" id="navbar-navlist">
+          <li className="nav-item">
+            <a className="nav-link" href="#home">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#about">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#resume">
+              Resume
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#portfolio">
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#services">
+              Services
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#blog">
+              Blog
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/*end collapse*/}
+    </div>
+    {/*end container*/}
+  </nav>
+  );
+}
+
+export default Navbar;
