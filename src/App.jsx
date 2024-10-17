f25d7f6a2e627a54c945c044661a9259a5dd25a8
szeedneigh <sidneyjohn1704@gmail.com>
2024-10-18 00:16:22 +0800

    Added and modified the services components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Skills from './components/Skills.jsx'
 import Resume from './components/Resume.jsx';
 import Portfolio from './components/Portfolio.jsx';
 import Partners from './components/Partners.jsx';
+import Services from './components/Services.jsx';
 
 
 
@@ -20,137 +21,8 @@ function App() {
       <Resume />
       <Portfolio />
       <Partners />
+      <Services />
       
-
-      
-      {/* start services */}
-      <section className="section overflow-hidden" id="services">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="section-title text-center mb-4 pb-2">
-                <span className="sub-title text-uppercase">Services</span>
-                <h3 className="title">My Services</h3>
-              </div>
-            </div>
-            {/* end col */}
-          </div>
-          {/* end row */}
-          <div className="row">
-            <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
-              <div className="service-wrapper rounded position-relative text-center border border-footer p-4 pt-5 pb-5">
-                <div className="icon text-primary">
-                  <i data-feather="layers" className="fea icon-md" />
-                </div>
-                <div className="content mt-4">
-                  <h5 className="title">Graphics Design</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    The purpose of lorem ipsum is to create a natural looking
-                    block of textwhen looking at its layout.
-                  </p>
-                </div>
-                <div className="big-icon">
-                  <i data-feather="layers" className="fea icons" />
-                </div>
-              </div>
-            </div>
-            {/*end col*/}
-            <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
-              <div className="service-wrapper rounded position-relative text-center border border-footer p-4 pt-5 pb-5">
-                <div className="icon text-primary">
-                  <i data-feather="box" className="fea icon-md" />
-                </div>
-                <div className="content mt-4">
-                  <h5 className="title">Web Design</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    It is a long established fact that a reader will be
-                    distracted by the when looking at its layout.
-                  </p>
-                </div>
-                <div className="big-icon">
-                  <i data-feather="box" className="fea icons" />
-                </div>
-              </div>
-            </div>
-            {/*end col*/}
-            <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
-              <div className="service-wrapper rounded position-relative text-center border border-footer p-4 pt-5 pb-5">
-                <div className="icon text-primary">
-                  <i data-feather="search" className="fea icon-md" />
-                </div>
-                <div className="content mt-4">
-                  <h5 className="title">App Research</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    It is a long established fact that a reader will be
-                    distracted by the when looking at its layout.
-                  </p>
-                </div>
-                <div className="big-icon">
-                  <i data-feather="search" className="fea icons" />
-                </div>
-              </div>
-            </div>
-            {/*end col*/}
-            <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
-              <div className="service-wrapper rounded position-relative text-center border border-footer p-4 pt-5 pb-5">
-                <div className="icon text-primary">
-                  <i data-feather="paperclip" className="fea icon-md" />
-                </div>
-                <div className="content mt-4">
-                  <h5 className="title">Branding</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    It is a long established fact that a reader will be
-                    distracted by the when looking at its layout.
-                  </p>
-                </div>
-                <div className="big-icon">
-                  <i data-feather="paperclip" className="fea icons" />
-                </div>
-              </div>
-            </div>
-            {/*end col*/}
-            <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
-              <div className="service-wrapper rounded position-relative text-center border border-footer p-4 pt-5 pb-5">
-                <div className="icon text-primary">
-                  <i data-feather="layout" className="fea icon-md" />
-                </div>
-                <div className="content mt-4">
-                  <h5 className="title">Responsive Web</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    It is a long established fact that a reader will be
-                    distracted by the when looking at its layout.
-                  </p>
-                </div>
-                <div className="big-icon">
-                  <i data-feather="layout" className="fea icons" />
-                </div>
-              </div>
-            </div>
-            {/*end col*/}
-            <div className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
-              <div className="service-wrapper rounded position-relative text-center border border-footer p-4 pt-5 pb-5">
-                <div className="icon text-primary">
-                  <i data-feather="navigation" className="fea icon-md" />
-                </div>
-                <div className="content mt-4">
-                  <h5 className="title">Trendy Work</h5>
-                  <p className="text-muted mt-3 mb-0">
-                    It is a long established fact that a reader will be
-                    distracted by the when looking at its layout.
-                  </p>
-                </div>
-                <div className="big-icon">
-                  <i data-feather="navigation" className="fea icons" />
-                </div>
-              </div>
-            </div>
-            {/*end col*/}
-          </div>
-          {/* end row */}
-        </div>
-        {/* end container */}
-      </section>
-      {/* end section */}
       {/* start testimony */}
       <section className="section bg-light overflow-hidden" id="testimonial">
         <div className="container">
