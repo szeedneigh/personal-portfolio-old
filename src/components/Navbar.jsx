1f08202e169ad0c1ef1e86ec38a3478668fa9b05
szeedneigh <sidneyjohn1704@gmail.com>
2024-10-18 22:52:25 +0800

    Added my logo and modified the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,53 @@
- 
-function Navbar() {
+import { useEffect } from 'react';
+
+const Navbar = () => {
+  useEffect(() => {
+    // Smooth scrolling
+    const navLinks = document.querySelectorAll('#navbar-navlist a');
+    navLinks.forEach(link => {
+      link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop - 70,
+            behavior: 'smooth'
+          });
+        }
+        
+        // Close mobile menu after clicking
+        const navbarCollapse = document.getElementById('navbarSupportedContent');
+        if (navbarCollapse.classList.contains('show')) {
+          navbarCollapse.classList.remove('show');
+        }
+      });
+    });
+
+    // Navbar scroll behavior
+    const handleScroll = () => {
+      const navbar = document.getElementById('navbar');
+      if (window.scrollY > 50) {
+        navbar.classList.add('nav-sticky');
+      } else {
+        navbar.classList.remove('nav-sticky');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav
-      id="navbar"
-      className="navbar navbar-expand-lg nav-light fixed-top sticky"
-    >
+    <nav id="navbar" className="navbar navbar-expand-lg nav-light fixed-top sticky">
       <div className="container">
         <a className="navbar-brand" href="#">
           <span className="logo-light-mode">
-            <img src="images/logo-dark.png" className="l-dark" alt="" />
-            <img src="images/logo-light.png" className="l-light" alt="" />
+            <img src="/images/logoDark.png" className="l-dark" alt="" />
+            <img src="/images/logoLight.png" className="l-light" alt="" />
           </span>
           <img src="images/logo-light.png" className="logo-dark-mode" alt="" />
         </a>
@@ -22,50 +60,37 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <i data-feather="menu" />
+          <i data-feather="menu"></i>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0" id="navbar-navlist">
             <li className="nav-item">
-              <a className="nav-link" href="#home">
-                Home
-              </a>
+              <a className="nav-link" href="#home">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#about">
-                About
-              </a>
+              <a className="nav-link" href="#about">About</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#resume">
-                Resume
-              </a>
+              <a className="nav-link" href="#resume">Resume</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#portfolio">
-                Portfolio
-              </a>
+              <a className="nav-link" href="#portfolio">Portfolio</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#services">
-                Services
-              </a>
+              <a className="nav-link" href="#services">Services</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#blog">
-                Blog
-              </a>
+              <a className="nav-link" href="#blog">Blog</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#contact">
-                Contact
-              </a>
+              <a className="nav-link" href="#contact">Contact</a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
